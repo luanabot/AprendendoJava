ba80e123aaf3c2e7784c20c15bbb61f41b1dcdf7
luana almeida <luanabotaro56@gmail.com>
2024-04-06 15:28:19 -0300

    testejava

---
+++ src/OlaMundoJava.java
@@ -0,0 +1,29 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class OlaMundoJava extends JFrame{
+    private JPanel painelMain;
+    private JTextField textName;
+    private JButton btnClick;
+
+    public OlaMundoJava () {
+        btnClick.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showConfirmDialog(btnClick,textName.getText()+",Hello ");
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        OlaMundoJava h=new OlaMundoJava();
+        h.setContentPane(h.painelMain);
+        h.setTitle("Hello");
+        h.setBounds(600,200,200,200);
+      //  h.setSize(300,400);
+        h.setVisible(true);
+        h.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+}
